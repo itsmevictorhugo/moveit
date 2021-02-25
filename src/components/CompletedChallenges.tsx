421c1eb22956b47dab1a789c514dd15034a61b8a
Victor Hugo <csivictorhugo@gmail.com>
2021-02-25 07:02:01 -0300

    component: countdown

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/components/CompletedChallenges.module.css';
 
 export function CompletedChallenges() {
   return(
-    <div className={styles.CompletedChallengesContainer}>
+    <div className={styles.completedChallengesContainer}>
       <span>Desafios completos</span>
       <span>5</span>
     </div>
