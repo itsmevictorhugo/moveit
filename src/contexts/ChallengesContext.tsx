bc0b25de3b83dfec13976cb18e8a2aa6b20b086d
Victor Hugo <csivictorhugo@gmail.com>
2021-02-26 06:28:11 -0300

    leveling estructure

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,7 +16,7 @@ interface ChallengesContextData {
   levelUp: ( ) => void;
   startNewChallenge: ( ) => void;
   resetChallenge: ( ) => void;
-
+  completeChallenge: ( ) => void;
 }
 
 interface ChallengesProviderProps {
@@ -48,6 +48,25 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
   function resetChallenge(){
     setActiveChallenge(null);
   }
+
+  function completeChallenge() {
+    if (!activeChallenge) {
+      return; 
+    }
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if(finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+  }
   
   return (
     <ChallengesContext.Provider 
@@ -60,6 +79,7 @@ export function ChallengesProvider({ children } : ChallengesProviderProps) {
         startNewChallenge,
         activeChallenge,
         resetChallenge,
+        completeChallenge,
       }}
     >
       {children}
